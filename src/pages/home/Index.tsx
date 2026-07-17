00a8cca7c9a05edee937b91200c3858ff8c8988e
hayarant <hayarant@gmail.com>
2026-07-17 02:33:19 +0000

    feat:adicionado tela home placeholder

--- src/pages/home/Index.tsx
+++ src/pages/home/Index.tsx
@@ -3,9 +3,11 @@ import Button from '../../components/Button'
 
 const HomePage = () => {
   return (
-    <div>
-      <h3>Home Page!</h3>
-      <Button text="Logout" onClick={useLogout} />
+    <div className="flex flex-col gap-2 items-center justify-center w-screen h-screen">
+      <div className="flex flex-col gap-2 border rounded-md w-[400px] h-[400px] items-center bg-amber-500 border-amber-600">
+        <h3 className="text-lg font-bold w-full">Home Page!</h3>
+        <Button text="Logout" onClick={useLogout} />
+      </div>
     </div>
   )
 }
